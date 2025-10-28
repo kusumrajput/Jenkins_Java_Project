a4372638810e7a311050cba1f31537e018d64973
kusumrajput <154332817+kusumrajput@users.noreply.github.com>
2025-10-28 19:47:58 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,8 +3,8 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins Java Project!");
-        int result = addNumbers(5, 10);
-        System.out.println("Sum of 5 and 10 is: " + result);
+        int result = addNumbers(6, 10);
+        System.out.println("Sum of 6 and 10 is: " + result);
     }
 
     public static int addNumbers(int a, int b) {
